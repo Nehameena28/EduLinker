7f573f796dc13df17bd3bd76d2444936a1c1f213
NehaMeena-debug <nehashivameena@gmail.com>
2025-02-25 13:20:16 +0530

    update package.json in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,21 +4,21 @@ import Home from "./Component/Menus/Home";
 import About from "./Component/Menus/About"
 import Contact from "./Component/Menus/Contact"
 import Navbar from "./Component/Menus/Navbar"
-import NavbarSB from "./Component/Home/NavbarSB";
-import Buyer from "./Component/Home/Buyer";
-import Seller from "./Component/Home/Seller";
+// import NavbarSB from "./Component/Home/NavbarSB";
+// import Buyer from "./Component/Home/Buyer";
+// import Seller from "./Component/Home/Seller";
 import Login from "./Component/Enter/Login"
 import Signup from "./Component/Enter/Signup"
-import SellerProfile from "./Component/SellerDashboard/SellerProfile"
-import S_Upload from "./Component/SellerDashboard/S_Upload"
-import S_Sell from "./Component/SellerDashboard/S_Sell"
-import S_History from "./Component/SellerDashboard/S_History"
-import S_Payment from "./Component/SellerDashboard/S_payment"
-import BuyerProfile  from "./Component/BuyerDashboard/BuyerProfile";
-import B_Shop from "./Component/BuyerDashboard/B_Shop"
-import B_Payment from "./Component/BuyerDashboard/B_Payment"
-import B_History from "./Component/BuyerDashboard/B_History"
-import B_Purchased from "./Component/BuyerDashboard/B_Purchased"
+// import SellerProfile from "./Component/SellerDashboard/SellerProfile"
+// import S_Upload from "./Component/SellerDashboard/S_Upload"
+// import S_Sell from "./Component/SellerDashboard/S_Sell"
+// import S_History from "./Component/SellerDashboard/S_History"
+// import S_Payment from "./Component/SellerDashboard/S_payment"
+// import BuyerProfile  from "./Component/BuyerDashboard/BuyerProfile";
+// import B_Shop from "./Component/BuyerDashboard/B_Shop"
+// import B_Payment from "./Component/BuyerDashboard/B_Payment"
+// import B_History from "./Component/BuyerDashboard/B_History"
+// import B_Purchased from "./Component/BuyerDashboard/B_Purchased"
 
 
 function App() {
@@ -27,9 +27,9 @@ function App() {
   
   return (
     <>
-      {/* <Navbar></Navbar> */}
+      <Navbar></Navbar>
       {/* <SellerProfile/> */}
-      <BuyerProfile/> 
+      {/* /<BuyerProfile/>  */}
       <Routes>
       <Route path="/" element={<Home />}></Route>
         <Route path="/Home" element={<Home />}></Route>
@@ -37,7 +37,7 @@ function App() {
         <Route path="/Contact" element={<Contact />}></Route>
         <Route path="/Login" element={<Login />}></Route>
         <Route path="/Signup" element={<Signup />}></Route>
-        <Route path="/Buyer" element={<Buyer />}></Route>
+        {/* <Route path="/Buyer" element={<Buyer />}></Route>
         <Route path="/Seller" element={<Seller />}></Route>
         <Route path="/S_Upload" element={<S_Upload/>}></Route>
         <Route path="/S_Sell" element={<S_Sell />}></Route>
@@ -46,7 +46,7 @@ function App() {
         <Route path="/B_Shop" element={<B_Shop/>}></Route>
         <Route path="/B_Purchased" element={<B_Purchased/>}></Route>
         <Route path="/B_History" element={<B_History/>}></Route>
-        <Route path="/B_Payment" element={<B_Payment/>}></Route>
+        <Route path="/B_Payment" element={<B_Payment/>}></Route> */}
       </Routes>
     </>
   )
