5005db108631428dd69c704e213d16e7d967c4e4
NehaMeena-debug <nehashivameena@gmail.com>
2025-07-05 14:49:42 +0530

    Import route in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 
 require('dotenv').config();
-const noteRoutes = require("./routes/noteRoutes");
+// const noteRoutes = require("./routes/noteRoutes");
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
