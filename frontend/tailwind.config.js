44524e9ca06959834005c6c183ad42e0c8c01e35
Khushi Verma <khushivrma07@gmail.com>
2025-07-15 14:09:11 +0530

    changes in tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,3 +1,27 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {
+//       colors: {
+//         'custom-blue': 'rgb(31, 91, 120)',
+//         'custom-i-berry': 'rgb(221, 167, 123)',
+//         'custom-brown':'rgb(148,93,94)'
+//       },
+//     },
+//   },
+//   plugins: [
+//     require('tailwind-scrollbar-hide') // 👈 add this line
+//   ],
+// }
+
+
+
+import tailwindScrollbarHide from 'tailwind-scrollbar-hide';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -9,12 +33,9 @@ export default {
       colors: {
         'custom-blue': 'rgb(31, 91, 120)',
         'custom-i-berry': 'rgb(221, 167, 123)',
-        'custom-brown':'rgb(148,93,94)'
+        'custom-brown': 'rgb(148,93,94)',
       },
     },
   },
-  plugins: [
-    require('tailwind-scrollbar-hide') // 👈 add this line
-  ],
-}
-
+  plugins: [tailwindScrollbarHide],
+};
