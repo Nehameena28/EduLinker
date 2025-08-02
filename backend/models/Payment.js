c3c1a6f9266d3d9854f755f7b4854733197e5abf
Khushi Verma <khushivrma07@gmail.com>
2025-08-02 10:58:23 +0530

    update the model of payment

--- backend/models/Payment.js
+++ backend/models/Payment.js
@@ -1,20 +1,53 @@
+// const mongoose = require("mongoose");
+
+// const paymentSchema = new mongoose.Schema({
+//   userName: String,
+//   userEmail: String,
+//   userPhone: String,
+//   items: [
+//     {
+//       title: String,
+//       description: String,
+//       price: Number,
+//       category: String
+//     }
+//   ],
+//   razorpay_order_id: String,
+//   razorpay_payment_id: String,
+//   razorpay_signature: String,
+// }, { timestamps: true });
+
+// module.exports = mongoose.model("Payment", paymentSchema);
+
+
+
+
 const mongoose = require("mongoose");
 
 const paymentSchema = new mongoose.Schema({
-  userName: String,
-  userEmail: String,
-  userPhone: String,
+  user: {
+    name: String,
+    email: String,
+    phone: String,
+  },
   items: [
     {
       title: String,
       description: String,
       price: Number,
-      category: String
-    }
+      category: String,
+      pdf: String,
+      cover: String,
+    },
   ],
   razorpay_order_id: String,
   razorpay_payment_id: String,
   razorpay_signature: String,
-}, { timestamps: true });
+  amountPaid: Number,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
 module.exports = mongoose.model("Payment", paymentSchema);
