2fe27e6ec6a756682507b68fb2a3d57ad027718e
Khushi Verma <khushivrma07@gmail.com>
2025-08-14 14:29:06 +0530

    update the ui of about page

--- frontend/src/Component/Menus/About.jsx
+++ frontend/src/Component/Menus/About.jsx
@@ -18,15 +18,15 @@ const About = () => {
           backgroundPosition: "center",
         }}
       >
-        <div className="absolute inset-0 bg-black bg-opacity-20"></div>
+        <div className="absolute inset-0 bg-black bg-opacity-50"></div>
         <div className="relative z-10">
-          <h1
-            className="text-5xl font-extrabold mb-4 text-white drop-shadow-md"
-             data-aos="fade-up"
-            
+          {/* <h1
+            className="text-5xl font-extrabold mb-4 text-white drop-shadow-lg"
+            style={{ textShadow: '2px 2px 4px rgba(0,0,0,0.8)' }}
+            data-aos="fade-up"
           >
             Learn. Share. Grow.
-          </h1>
+          </h1> */}
 
          
           <p
